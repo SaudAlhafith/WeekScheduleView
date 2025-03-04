210f8c6b2f3ea5b9cc22afd0d5e09bc904d39edc
SaudAlhafith <128002196+SaudAlhafith@users.noreply.github.com>
2025-03-04 15:12:14 +0300

    no need for -1

--- Example/WeekScheduleExample/ContentView.swift
+++ Example/WeekScheduleExample/ContentView.swift
@@ -16,7 +16,7 @@ struct ContentView: View {
     var body: some View {
         VStack {
             WeekScheduleView(entries: example)
-                .scheduleDays(.weekdays, daySpacing: 4, dayFont: .system(size: 10), dayStyle: .wide) // Show weekdays with some styling
+                .scheduleDays(.custom(days: [.sunday, .monday, .tuesday, .wednesday, .thursday]), daySpacing: 4, dayFont: .system(size: 10), dayStyle: .wide) // Show weekdays with some styling
                 .timelineIndicator(.red)
                 .timelineRange(.entriesOnly) // Show 24-hour timeline
                 .entryHeight(50, 80) // Set normal and expanded entry height
